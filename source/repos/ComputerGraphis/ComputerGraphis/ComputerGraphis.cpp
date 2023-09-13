1aa8ea1056d28c80b3afef5157e95284f7ae38f4
fkdl0048 <fkdl000048@gmail.com>
2023-09-14 04:43:05 +0900

    Feat: Project Init

---
+++ source/repos/ComputerGraphis/ComputerGraphis/ComputerGraphis.cpp
@@ -0,0 +1,33 @@
+﻿// ComputerGraphis.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <vgl.h>
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_TRIANGLES);
+	glVertex2f(-0.5, -0.5);
+	glVertex2f(0.5, -0.5);
+	glVertex2f(-0.5, 0.5);
+	glVertex2f(0.5, -0.5);
+	glVertex2f(0.5, 0.5);
+	glVertex2f(-0.5, 0.5);
+	glEnd();
+	glFlush();
+}
+
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowSize(512, 512);
+	glutCreateWindow("Hello GL");
+
+	glutDisplayFunc(display);
+	glutMainLoop();
+
+	return 0;
+}
