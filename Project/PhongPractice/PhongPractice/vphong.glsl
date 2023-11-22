2461964cf1cfb5afeefb94754dbd8a7f747adfe2
fkdl0048 <fkdl000048@gmail.com>
2023-11-22 14:32:09 +0900

    Docs: Phong Partice

---
+++ Project/PhongPractice/PhongPractice/vphong.glsl
@@ -0,0 +1,22 @@
+#version 330
+
+in  vec4 vPosition;
+in  vec4 vColor;
+in  vec3 vNormal;
+out vec4 color;
+
+uniform mat4 uProjMat;
+uniform mat4 uModelMat;
+
+void main()
+{
+	gl_Position  = uProjMat * (uModelMat * vPosition);
+	gl_Position *= vec4(1,1,-1,1);	// z축 방향이 반대임
+
+
+
+
+
+    
+	color = vColor;
+}
